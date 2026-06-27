c8ffbc9728cebb498761497d45335f7e39c3e3ec
randika-913 <randikasampath2020@gmail.com>
2026-06-27 16:04:57 +0530

    fifth

--- js/app.js
+++ js/app.js
@@ -62,3 +62,91 @@
 //     console.log("false");
 // }
 
+//let x = 10;
+
+//let y = 20;
+
+//if(x > y){
+   // console.log("X is greater than Y");
+//}else if(x < y){
+//    console.log("Y is greater than X");
+//}else{
+ //   console.log("X and Y are Equal")
+//}
+
+//let day = 2;
+
+//switch(day){
+//    case 1:
+//        console.log("Monday");
+//       break;
+//    case 2:
+//        console.log("Tuesday");
+//        break;
+//    case 3:
+//        console.log("Wednesday");
+//        break;
+//    case 4:
+//       console.log("Thursday");
+//        break;
+//    case 5:
+//        console.log("Friday");
+//        break;
+//    case 6:
+//        console.log("Saturday");
+ //       break;
+//    case 7:
+//        console.log("Sunday");
+//        break;
+//    default:
+//        console.log("Invalid Day");
+//}
+
+//for (let i = 0; i < 10; i++) {
+//    console.log(i);  
+//}
+
+//let i = 0;
+
+//while (i <= 10) {
+//    console.log(i);
+//    i++
+//}
+
+
+//let i = 0;
+
+//do{
+//    console.log(i);
+//   i++
+//}while(i<=10);
+
+//function
+
+//function add(x,y) {
+//    return x+y;
+//}
+
+//let result = add(10, 20);
+//   console.log(result);
+
+//arrays
+
+//let numbers = [1, true, 3, "saman", 5, "ranil", "sunil"];
+
+//for (let i = 0; i < numbers.length; i++) {
+//    console.log(numbers[i]);
+    
+//}
+
+//object
+
+
+let customer = {
+    name: "Saman",
+    age: 18,
+    isAfterAL: false,
+    salary: 7500.0
+}
+
+console.log(customer);
