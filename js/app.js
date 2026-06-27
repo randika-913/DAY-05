3b3a6cab4ac9d936ddd12c2927cb345ed6612d63
randika-913 <randikasampath2020@gmail.com>
2026-06-27 14:51:33 +0530

    fourth

--- js/app.js
+++ js/app.js
@@ -1,10 +1,64 @@
-let name = "Hello";
+// let name = "Saman";
 
-let age = 18;
+// let age = 18;
 
-name = "hi";
+// name = "ranil";
 
-console.log(name);
-console.log(age);
+// console.log(name);
+// console.log(age);
 
-alert("kohomada machan ubata?");
+// alert("Hello icet");
+
+
+// String name = "10";
+
+// ---------------------------------
+
+// let name = "saman";
+// let age = 18;
+// let isAfterAL = false;
+// let customers = [];
+// let customer = {};
+// let salary = 7500.0;
+
+// console.log(typeof name);
+
+// --------------------------------------------
+//  arithmetic operators
+
+// + | - | * | /
+
+// let x = 10;
+// let y = 20;
+
+// let z = x / y;
+
+// console.log(z);
+
+// -------------------------------------------
+
+//  comparison operators
+
+// > | < | >= | <= | == | !=
+// >== | <== | === | !==
+
+
+// let x = 11;
+
+// let y = "11";
+
+// console.log(x===y);
+
+
+// -------------------------------------------
+
+//  logical operators
+
+//  && | || | !
+
+// if (!false) {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
+
